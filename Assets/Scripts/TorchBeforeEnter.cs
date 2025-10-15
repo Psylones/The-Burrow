1687951700591ccff45b5734aa92184e7aee6644
Psylones <map60944@gmail.com>
2025-10-15 14:59:55 +1300

    Completed for playtests

--- Assets/Scripts/TorchBeforeEnter.cs
+++ Assets/Scripts/TorchBeforeEnter.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 using System.Collections.Generic;
 using System.Collections;
 using System;
-using UnityEditor.Rendering;
 using TMPro;
 
 public class TorchBeforeEnter : MonoBehaviour
